b1b488b74c79b05eb823dbdfdf35e90078316165
Javier Carpio <30840593+javiercarpio57@users.noreply.github.com>
2020-10-21 15:40:21 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.scss'
+
+export default class App extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Hola, soy un sample.</h1>
+            </div>
+        )
+    }
+}
